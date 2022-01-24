b1b8b7d49cecdd2eeabf3a6707531d0618d57791
unknown <harshithxworkz@gmail.com>
2022-01-24 11:15:41 +0530

    wapperclss using forloop of float

---
+++ Stringfloat.java
@@ -0,0 +1,8 @@
+class Stringfloat{
+  public static void main(String[]harshi){
+  for(byte index=0;index<harshi.length;index++){
+	     float n= Float.parseFloat(harshi[index]);
+         System.out.println(n); 
+      }
+   }
+ }
