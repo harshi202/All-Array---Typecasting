ab87f6ca330991681b2871a047ebbfc74435049f
unknown <harshithxworkz@gmail.com>
2022-01-24 11:18:55 +0530

    wapperclss using forloop of boolean

---
+++ Stringboolean.java
@@ -0,0 +1,8 @@
+class Stringboolean{
+  public static void main(String[]harshi){
+  for(int index=0;index<harshi.length;index++){
+  boolean h = Boolean.parseBoolean(harshi[index]); 
+	    System.out.print(h);
+      }
+   }
+ }
