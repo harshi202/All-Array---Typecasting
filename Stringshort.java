e4f67102f1b97a9de4300de1daf8c02dd9381cf7
unknown <harshithxworkz@gmail.com>
2022-01-24 09:01:18 +0530

    wapperclss using for loop of short

---
+++ Stringshort.java
@@ -0,0 +1,8 @@
+class Stringshort{
+  public static void main(String[]harshi){
+  for(short index=0;index<harshi.length;index++){
+	     Short n= Short.parseShort(harshi[index]);
+         System.out.println(n); 
+      }
+   }
+ }
