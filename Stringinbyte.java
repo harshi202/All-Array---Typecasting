32220d4ebf362670577600f48c3318ef6d7da464
unknown <harshithxworkz@gmail.com>
2022-01-24 09:00:09 +0530

    wapperclss using for loop of int

---
+++ Stringinbyte.java
@@ -0,0 +1,8 @@
+class Stringinbyte{
+  public static void main(String[]harshi){
+  for(byte index=0;index<harshi.length;index++){
+	    byte H = Byte.parseByte(harshi[index]);
+        System.out.println(H); 
+      }
+   }
+ } 
