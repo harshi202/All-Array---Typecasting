4fe9f555f4b517a038ce2d827aa2f369fbdc9e87
unknown <harshithxworkz@gmail.com>
2022-01-24 11:14:08 +0530

    wapperclss using forloop oflong

---
+++ Stringlong.java
@@ -0,0 +1,8 @@
+class Stringlong{
+  public static void main(String[]harshi){
+  for(int i=0;i<harshi.length;i++){
+	     long n = Long.parseLong(harshi[i]);
+         System.out.println(n); 
+      }
+   }
+ }
