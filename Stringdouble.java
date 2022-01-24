341335f67749e365dc9a1cc85a9a5a26eaea144c
unknown <harshithxworkz@gmail.com>
2022-01-24 11:17:29 +0530

    wapperclss using forloop of double

---
+++ Stringdouble.java
@@ -0,0 +1,8 @@
+class Stringdouble{
+  public static void main(String[]harshi){
+  for(short index=0;index<harshi.length;index++){
+	  double n= Double.parseDouble(harshi[index]);
+      System.out.println(n);
+      }
+   }
+ }
