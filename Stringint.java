3da186e36f02571f497484b4f7f3727d4e4117b1
unknown <harshithxworkz@gmail.com>
2022-01-24 09:02:10 +0530

    wapperclss using for loop of int

---
+++ Stringint.java
@@ -0,0 +1,8 @@
+class Stringint{
+  public static void main(String[]args){
+  for(int i=0;i<args.length;i++){
+	 int n= Integer.parseInt(args[i]);
+      System.out.println(n);	 
+      } 
+   }
+ }
